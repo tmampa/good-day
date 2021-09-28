3d10f88c86f22b46452e47777684736ad09ec8a0
tmampa <tmampa94@gmail.com>
2021-09-28 13:43:11 +0200

    Add icons

--- src/index.js
+++ src/index.js
@@ -60,15 +60,15 @@ function weatherDetails(info){
         const {temp, feels_like, humidity} = info.main;
 
         if(id == 800){
-            wIcon.src = "icons/clear.svg";
+            wIcon.src = "icons/clear-day.svg";
         }else if(id >= 200 && id <= 232){
-            wIcon.src = "icons/storm.svg";  
+            wIcon.src = "icons/thunderstorms.svg";  
         }else if(id >= 600 && id <= 622){
             wIcon.src = "icons/snow.svg";
         }else if(id >= 701 && id <= 781){
             wIcon.src = "icons/haze.svg";
         }else if(id >= 801 && id <= 804){
-            wIcon.src = "icons/cloud.svg";
+            wIcon.src = "icons/cloudy.svg";
         }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
             wIcon.src = "icons/rain.svg";
         }
